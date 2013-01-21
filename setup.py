2d27bfc8c63bce2b667e83d30711a7aee82eef01
viktorg <viktor.granholm@scilifelab.se>
2013-01-21 09:47:44 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name='granholm',
+version='0.1dev',
+author='Viktor Granholm',
+author_email='viktor.granholm@scilifelab.se',
+url='https.//github.com/viktorg/granholm',
+packages=['granholm',],
+license='GPLv3',
+long_description=open('README.txt').read(),
+)
