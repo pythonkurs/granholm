afd8c38760b15036f39d150350eb124503d4fb96
viktorg <viktor.granholm@scilifelab.se>
2013-01-25 11:14:39 +0100

    Moved things to granholm/__init__.py

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 
-import requests
-from lxml import etree
+from granholm import getting_data
+
 
 def main():
-    '''
-    Import NY subway escalator outage data, print the fraction of those that are in 'REPAIR'
-    '''
-    # Download and initialize up XML-tree and variables
-    r = requests.get('http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml')
-    root = etree.XML(r.content)
-    num_of_repair = 0.0
-    total_num = 0.0
-    # Iterate through outages in XML file
-    for reason_elem in root.findall('outage/reason'):
-        total_num += 1
-        if reason_elem.text == 'REPAIR':
-            num_of_repair += 1
-    # Calculate fraction and print
-    repair_fraction = num_of_repair/total_num
-    print 'The fraction of escalator outages with reason \"REPAIR\": %.3f' % (repair_fraction)
+    getting_data()
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup
 setup(
-name='granholm',
-version='0.1dev',
-author='Viktor Granholm',
-author_email='viktor.granholm@scilifelab.se',
-url='https.//github.com/viktorg/granholm',
-packages=['granholm',],
-license='GPLv3',
-long_description=open('README.txt').read(),
+    name='granholm',
+    version='0.1dev',
+    author='Viktor Granholm',
+    author_email='viktor.granholm@scilifelab.se',
+    url='https.//github.com/viktorg/granholm',
+    packages=['granholm',],
+    license='GPLv3',
+    long_description=open('README.txt').read(),
 )
