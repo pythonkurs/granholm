22fe9d207b6576fc101d8a4109db3c93c1818983
viktorg <viktor.granholm@scilifelab.se>
2013-02-12 13:30:05 +0100

    session 4 script

--- granholm/session4.py
+++ granholm/session4.py
@@ -5,18 +5,19 @@ from dateutil import parser
 import requests
 import getpass
 import json
+from collections import Counter
 
 class ReadRepos(object):
+    '''Reads github repositories into a pandas dataframe'''
 
-    def __init__(username, password):
+    def __init__(self, username, password):
         # Github credentials
         self.username = username
         self.password = password
 
-    def read_github_commits():
+    def read_github_commits(self):
         '''Produce a DataFrame with commit messages of Pythonkurs repositories'''
-        print 'Read through the pythonkurs repositories, store commit messages and times.'
-        print 'Repository:'
+        print 'Reading repositories:',
         # Initialize dictionary to make dataframe
         repo_dict = {}
         # Read Github API
@@ -24,7 +25,7 @@ class ReadRepos(object):
         repos =  json.loads(response.content)  # Couldn't get the json-method working for response, so I use the json module.
         for repo in repos:
             repo_name = repo['name']
-            print repo_name
+            print repo_name,
             commit_response = requests.get("https://api.github.com/repos/pythonkurs/%s/commits" % (repo_name), auth=(self.username, self.password))
             commits = json.loads(commit_response.content)
             messages = []
@@ -32,21 +33,50 @@ class ReadRepos(object):
             # It seems, if at least one commit is made, commits has the type list
             if type(commits) == list:
                 for commit in commits:
-                    times.append(commit['commit']['author']['date'])
+                    times.append(parser.parse(commit['commit']['author']['date']))
                     messages.append(commit['commit']['message'])
                 repo_dict[repo_name] = Series(messages, index=times)
             else:
                 # No commits...
                 pass
+        print ''
         self.df = DataFrame(repo_dict)
 
-class MostCommonHour(object):
+
+class DataFrameOfCommits(object):
+    '''Takes a dataframe of github commits, calculates most common dates etc.'''
+
+    def __init__(self, dataframe):
+        self.df = dataframe
+        self.weekday_list = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
+
+    def most_common_day_for_commits(self):
+        weekdays = []
+        for row in self.df.iterrows():
+            weekdays.append(row[0].weekday())
+        c = Counter(weekdays)
+        weekday_int = c.most_common()[0][0]  # The index of the most common weekday
+        print 'The most common weekday for commits is: %s' % self.weekday_list[weekday_int]
+
+    def most_common_hour_for_commits(self):
+        hours = []
+        for row in self.df.iterrows():
+            hours.append(row[0].hour)
+        c = Counter(hours)
+        hour = c.most_common()[0][0]  # The int value of the most common hour
+        print 'The most common hour for commits is: %s' % hour
 
 
 def main():
+    # Get github pass
     password = getpass.getpass()
-    repositories = ReadRepos('viktorg', password)
+    # Get github commits
+    repos = ReadRepos('viktorg', password)
     repos.read_github_commits()
+    # Count statistics from github commtis
+    df_of_commits = DataFrameOfCommits(repos.df)
+    df_of_commits.most_common_day_for_commits()
+    df_of_commits.most_common_hour_for_commits()
     
 
 if __name__ == '__main__':
